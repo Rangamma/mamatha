99617087dc079efdf758a66beb9783534bec1819
Rangamma <104354678+Rangamma@users.noreply.github.com>
2022-04-27 14:41:56 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,18 @@
+[02:28 pm] Deeksha (Guest)
+/**
+
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+
+ */
+
+public class HelloWorld {​​​​​
+
+  public static void main(String[] args) {​​​​​
+
+    System.out.println("Hello, World");
+
+  }​​​​​
+
+}​​​​​
+
+
